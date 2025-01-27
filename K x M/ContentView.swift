e6fcfce46bccc2456037316ba19ef511610222e8
hoover13 <hoovermtk25@gmail.com>
2025-01-27 13:23:43 -0500

    Initial Commit

---
+++ K x M/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  K x M
+//
+//  Created by Min Thu Khine on 1/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ K x M/K_x_MApp.swift
@@ -0,0 +1,17 @@
+//
+//  K_x_MApp.swift
+//  K x M
+//
+//  Created by Min Thu Khine on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct K_x_MApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
