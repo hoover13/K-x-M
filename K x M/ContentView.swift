ee12854c78d8f6a2fd904c7d82b3cb63c77b4734
hoover13 <hoovermtk25@gmail.com>
2025-01-27 13:57:55 -0500

    - change background color

--- K x M/ContentView.swift
+++ K x M/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            
+            Button {
+                
+            } label: {
+                Image(systemName: "person.crop.circle")
+                    .foregroundStyle(.white)
+                    .font(.system(size: 100))
+            }
         }
-        .padding()
     }
 }
 
